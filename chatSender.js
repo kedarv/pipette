070a91113cc8f9b9c9d5132413c7610fb0c1aaf1
Kedar <kedar54@gmail.com>
2016-06-14 20:46:35 -0400

    change sys in favor for utils

--- chatSender.js
+++ chatSender.js
@@ -1,6 +1,6 @@
-var sys = require('sys')
+var util = require('util')
 var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { sys.puts(stderr); sys.puts(stdout); }
+function puts(error, stdout, stderr) { util.puts(stderr); util.puts(stdout); }
 
 function sendMessage(txt, guid) {
 	var cmd = 'osascript tools/send_message.applescript "'+guid+'" "'+txt+'"';
